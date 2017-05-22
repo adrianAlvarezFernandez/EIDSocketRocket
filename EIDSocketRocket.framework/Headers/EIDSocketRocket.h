f09a4e7cff1c14bf13067a1044285fa5da763b2c
Adrian Alvarez <adrian@electronicid.eu>
2017-05-22 09:34:02 +0200

    first commit

---
+++ EIDSocketRocket.framework/Headers/EIDSocketRocket.h
@@ -0,0 +1,20 @@
+//
+//  EIDSocketRocket.h
+//  EIDSocketRocket
+//
+//  Created by Adrián Álvarez Fernández on 22/05/2017.
+//  Copyright © 2017 Electronic ID. All rights reserved.
+//
+
+#import <SocketRocket/SRWebSocket.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for EIDSocketRocket.
+FOUNDATION_EXPORT double EIDSocketRocketVersionNumber;
+
+//! Project version string for EIDSocketRocket.
+FOUNDATION_EXPORT const unsigned char EIDSocketRocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EIDSocketRocket/PublicHeader.h>
+
+
